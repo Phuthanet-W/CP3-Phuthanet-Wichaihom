42fdeec9733031593a38630088c2f98b33f0be65
Phuthanet-W <78694197+Phuthanet-W@users.noreply.github.com>
2021-02-09 22:33:02 +0700

    Exercirse4_Phuthanet-W
    Homework ex4

---
+++ Exercires4_Phuthanet_W.py
@@ -0,0 +1,13 @@
+Class1 = "Foundation English :"
+Class2 = "General Business :"
+Class3 = "Introduction to Computer Systems :"
+Class4 = "Computer Programming :"
+ScoreC1 = 70.35
+ScoreC2 = 80
+ScoreC3 = 79.99
+ScoreC4 = 100
+print("---Your Score---")
+print(Class1, ScoreC1)
+print(Class2, ScoreC2)
+print(Class3, ScoreC3)
+print(Class4, ScoreC4)
