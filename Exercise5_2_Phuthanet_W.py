2598756023ed55b1c53876a17ad23d009f7f2ffe
Phuthanet-W <78694197+Phuthanet-W@users.noreply.github.com>
2021-02-11 23:20:47 +0700

    Add meter per sec

--- Exercise5_2_Phuthanet_W.py
+++ Exercise5_2_Phuthanet_W.py
@@ -1,5 +1,6 @@
 print("Velocity calculation")
 s = int(input("Please fill distance in kilometer(kg) : "))
 t = int(input("Please fill time in hour (hr)"))
-v = s/t
-print("Velocity is ", v,"km/hr")
+v = s/t #km/hr
+v1 = (s*1000)/(t*3600)
+print("Velocity is ", v,"km/hr","or",v1,"m/s")
