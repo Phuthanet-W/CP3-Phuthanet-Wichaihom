6e83bf6b5f4bd2d91481168e1598f837cb38b0bd
Phuthanet-W <78694197+Phuthanet-W@users.noreply.github.com>
2021-02-11 22:59:42 +0700

    Exercise5_1
    calculation number program

---
+++ Exercise5_1_Phuthanet_W.py
@@ -0,0 +1,11 @@
+print("Math class")
+x = int(input("Please fill first number : "))
+y = int(input("Please fill second number : "))
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+print(x,"+",y,"=",a)
+print(x,"-",y,"=",b)
+print(x,"*",y,"=",c)
+print(x,"/",y,"=",d)
