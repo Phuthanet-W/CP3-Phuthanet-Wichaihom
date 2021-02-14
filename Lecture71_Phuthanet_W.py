29d6ec2cba63a733a54c3c9e7a32649b71598676
Phuthanet-W <78694197+Phuthanet-W@users.noreply.github.com>
2021-02-14 19:19:59 +0700

    Lecture71_Phuthanet_W
    collection1

---
+++ Lecture71_Phuthanet_W.py
@@ -0,0 +1,20 @@
+menulist = []
+pricelist = []
+totalprice = int()
+
+def bill():
+  print("========FOOD DOG=======")
+  for number in range(len(menulist)):
+    print(menulist[number],"=",pricelist[number])
+while True:
+  menuName = input("Please Enter menu : ").capitalize()
+  if (menuName == "Exit"):
+    break
+  else:
+    menuprice = int(input("price :"))
+    menulist.append(menuName)
+    pricelist.append(menuprice)
+    totalprice += menuprice
+
+bill()
+print("Total price is " ,totalprice, "Bth")
