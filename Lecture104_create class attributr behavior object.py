cec99a00d7194dff947eaa6fcfd3bf3c58c828a2
Phuthanet-W <78694197+Phuthanet-W@users.noreply.github.com>
2021-02-18 20:41:32 +0700

    Lecture104_Phithanet_W
    Create OOP

---
+++ Lecture104_create class attributr behavior object.py
@@ -0,0 +1,19 @@
+class customer:
+    name = ""
+    lastname = ""
+    age = 0
+
+    def addcarf(self):
+        print("Added to ",self.name , self.lastname,"'s cart")
+customer1 = customer()
+customer1.name = "Phuthanet"
+customer1.lastname = ".W"
+customer1.addcarf()
+customer2 = customer()
+customer2.name = "Sorawit"
+customer2.lastname = ".R"
+customer2.addcarf()
+customer3 = customer()
+customer3.name = "Narakorn"
+customer3.lastname = ".M"
+customer3.addcarf()
